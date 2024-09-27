04bda6023cec2f3fadd420f6ca696e33269580b8
Bri <bcarey0803@gmail.com>
2024-09-27 16:01:40 -0400

    Added Initial Spring Boot Build

---
+++ vinyl-library/src/main/java/com/music/vinyl_library/VinylLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.music.vinyl_library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VinylLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VinylLibraryApplication.class, args);
+	}
+
+}
---
+++ vinyl-library/src/test/java/com/music/vinyl_library/VinylLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.music.vinyl_library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VinylLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
